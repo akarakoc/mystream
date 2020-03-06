57680e3e7d4413bd4da0c2205dccf4099a5f7cce
root <root@ip-172-31-44-107.us-east-2.compute.internal>
2020-03-06 09:24:38 +0000

    Configurations has done

--- mystream/streampage/admin.py
+++ mystream/streampage/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from streampage.models import Primitives,communityUsers,Communities,Datatypes,DatatypeFields,Posts,CommunityTags,DatatTypeTags,PostTags,UserTags,UserCircle
 
-# Register your models here.
+admin.site.register(Primitives)
+admin.site.register(communityUsers)
+admin.site.register(Communities)
+admin.site.register(Datatypes)
+admin.site.register(DatatypeFields)
+admin.site.register(Posts)
+admin.site.register(CommunityTags)
+admin.site.register(DatatTypeTags)
+admin.site.register(PostTags)
+admin.site.register(UserTags)
+admin.site.register(UserCircle)
+
