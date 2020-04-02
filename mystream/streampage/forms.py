0955e898a028e2999bb99a0a51ff98e322e6416c
root <root@ip-172-31-44-107.us-east-2.compute.internal>
2020-04-02 01:45:30 +0000

    advanced search

--- mystream/streampage/forms.py
+++ mystream/streampage/forms.py
@@ -428,13 +428,14 @@ class AddEnumaratedSearch(forms.Form):
         super(AddEnumaratedSearch, self).__init__(*args, **kwargs)
         ChoiceList = ["equals", "contains","not equal","not contain"]
         self.fields['Condition'] = forms.ChoiceField(choices=tuple(enumerate(ChoiceList)),label='')
-        self.fields['EnumaratedEntry'] = forms.ChoiceField(choices=tuple(enumerate(enum)),label='')
-        self.fields['EnumaratedEntry'].widget.attrs.update({'class': 'form-control'})
+        self.fields['EnumaratedSearchEntry'] = forms.ChoiceField(choices=tuple(enumerate(enum)),label='')
+        self.fields['EnumaratedSearchEntry'].widget.attrs.update({'class': 'form-control'})
         self.fields['Condition'].widget.attrs.update({'class': 'form-control'})
         contextName={}
         contextName['name']=name
         cnName = contextName.get(name,name)
         super(AddEnumaratedSearch, self).add_prefix(cnName)
+
         
 class AddLocationSearch(forms.Form):
     Operand = ["","AND", "OR"]
@@ -470,3 +471,16 @@ class posttypeList(forms.Form):
         name="Posttype"
         cnName = contextName.get(name,name)
         super(posttypeList, self).add_prefix(cnName)
+
+class searchList(forms.Form):	
+    def __init__(self, *args, **kwargs):
+        cmHash = kwargs.pop('cHash')
+        print(cmHash)
+        super(searchList, self).__init__(*args, **kwargs)
+        CommList = Communities.objects.filter(communityHash=cmHash)[0]
+        self.fields['searchEntry'] = forms.ModelChoiceField(queryset=CommList.datatypefields_set.all().order_by('name'),label='',to_field_name="name")
+        self.fields['searchEntry'].widget.attrs.update({'class': 'form-control'})
+        contextName={}
+        name="Search Items"
+        cnName = contextName.get(name,name)
+        super(searchList, self).add_prefix(cnName)
