24779c84f156c1acf84462d60aa82bd8ebf137dc
mateskarayol <melikeateskarayol@gmail.com>
2020-05-02 13:26:50 +0300

    Solve Cross Origin Problem between Annotation server and front-end #96

--- MyAnnotation/src/main/java/com/mystream/controller/AnnotationRestController.java
+++ MyAnnotation/src/main/java/com/mystream/controller/AnnotationRestController.java
@@ -1,11 +1,15 @@
 package com.mystream.controller;
 
 import com.mystream.dom.Constants;
+import com.mystream.dom.Selector;
+import com.mystream.dom.Target;
+import com.mystream.dom.TypeEnum;
 import com.mystream.param.AnnotationRequest;
 import com.mystream.param.AnnotationResponse;
 import com.mystream.dom.TextAnnotation;
 import com.mystream.service.AnnotationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,12 +24,18 @@ public class AnnotationRestController {
 	@Autowired
 	AnnotationService annotationService;
 
+	@CrossOrigin(origins = "http://127.0.0.1:8000/")
 	@RequestMapping(name = "/annotateText", produces = "application/json", method= RequestMethod.POST)
 	@ResponseBody
 	public AnnotationResponse annotateText(@RequestBody AnnotationRequest request) throws Exception {
 
 		TextAnnotation annotation = request.getTextAnno();
 		annotation.setContext(Constants.ANNOTATION_CONTEXT_URI);
+
+		for ( Target target : annotation.getTarget()) {
+			if(target.getSelector().equals(TypeEnum.FragmentSelector))
+				target.getSelector().setConformsTo(Constants.SELECTOR_FRAGMENT_CONFORMS_TO);
+		}
 
 		TextAnnotation anno = annotationService.saveTextAnnotation(request.getTextAnno());
 		AnnotationResponse response = new AnnotationResponse();
--- MyAnnotation/src/main/java/com/mystream/dom/Constants.java
+++ MyAnnotation/src/main/java/com/mystream/dom/Constants.java
@@ -3,4 +3,5 @@ package com.mystream.dom;
 public class Constants {
 
 	public static final String ANNOTATION_CONTEXT_URI = "http://www.w3.org/ns/anno.jsonld";
+	public static final String SELECTOR_FRAGMENT_CONFORMS_TO = "http://www.w3.org/TR/media-frags/";
 }
