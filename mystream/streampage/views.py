2b343be1027edd40151abe634d4da408ec3660a4
Ali Karakoc <ali.karakoc@boun.edu.tr>
2020-06-15 09:41:25 +0300

    Revert "Development"

--- mystream/streampage/views.py
+++ mystream/streampage/views.py
@@ -31,14 +31,13 @@ from streampage.models import Primitives,communityUsers,Communities,Datatypes,Da
 from django.core.serializers.json import DjangoJSONEncoder
 from django.db.models import Q
 from countryinfo import CountryInfo
-from unicode_tr import unicode_tr
 
 def saveTagSearch_view(src):
     SEARCHPAGE = src	
     PARAMS = {
 		"action":"wbsearchentities",
 		"format": "json",
-		"limit": "10",
+		"limit": "50",
         "language":"en",
 		"search": SEARCHPAGE
     }
@@ -215,7 +214,6 @@ def CreateCommunity_view(request):
     form = AddCommunity(request.POST, request.FILES)
     c_image=request.FILES.get("Community_Image")
     image_path=handle_uploaded_file(c_image)
-
     comm = Communities()
     comm.name = request.POST.get("Community_Name")
     comm.description = request.POST.get("Community_Description")
@@ -237,9 +235,9 @@ def CreateCommunity_view(request):
     comm.save()
     Tags = saveTag_view(request.POST.get("Community_Tags"))
     tagentry = CommunityTags()
-    relatedComm = Communities.objects.filter(communityHash=commhash)[0]
+    relatedComm = Communities.objects.filter(communityHash=commhash)[0] 
     tagentry.communityTag = relatedComm
-    tagentry.tagName = Tags["TITLE"]
+    tagentry.tagName = Tags["TITLE"] 
     tagentry.tagItem = Tags["ITEM"]
     tagentry.save()
 
@@ -260,6 +258,7 @@ def CreateCommunity_view(request):
             "hash": comm.communityHash
         }
     }
+
     ActivityStreams.objects.create(detail = description)
     return render(None, 'tagSearch.html', {'form' : "Community is created Successfully!"})
 
@@ -427,7 +426,7 @@ def searchTag_view(request):
     PARAMS = {
 		"action":"wbsearchentities",
 		"format": "json",
-		"limit": "10",
+		"limit": "50",
         "language":"en",
 		"search": SEARCHPAGE
     }
@@ -1813,10 +1812,8 @@ def communityLocationPageSearch_view(request):
         if request.GET.get('keyword'):
             if Communities.objects.all():
                 searchString = request.GET.get('keyword')
-                text_true = unicode_tr(searchString)
-                print(text_true.capitalize())
-                Community_List = Communities.objects.filter(communityCountry__icontains = text_true.capitalize()).order_by(
-                    '-communityCreationDate') | Communities.objects.filter(communityLocation__icontains=text_true.capitalize()).order_by(
+                Community_List = Communities.objects.filter(communityCountry__contains=searchString).order_by(
+                    '-communityCreationDate') | Communities.objects.filter(communityLocation__contains=searchString).order_by(
                     '-communityCreationDate')
                 Cuser = request.user
                 UserList = communityUsers.objects.filter(nickName=Cuser)[0]
